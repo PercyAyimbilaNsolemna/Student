796268a9133850be569b286bcb90f7b15d0cf9d3
master <pnayimbila@st.ug.edu.gh>
2023-03-27 16:03:40 +0000

    Added an __init__ method.

---
+++ Student.py
@@ -0,0 +1,5 @@
+class Student:
+    def __init__(self, name=None, studentID=None, courseList=None):
+        self.name = name
+        self.studentID = studentID
+        self.courseList = courseList
