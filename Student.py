b7c651889f30eb431ddfc5f1a62d034beb293ddf
master <pnayimbila@st.ug.edu.gh>
2023-03-27 16:05:08 +0000

    Added an __str__ method.

--- Student.py
+++ Student.py
@@ -3,3 +3,9 @@ class Student:
         self.name = name
         self.studentID = studentID
         self.courseList = courseList
+
+    #Creates an __str__ that makes it possible to pass an object created from this class to the print function
+    def __str__(self):
+        return "This is a Student class"
+
+        
