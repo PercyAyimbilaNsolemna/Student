744a0d240d741de15350788c6185ba8242a8960a
master <pnayimbila@st.ug.edu.gh>
2023-03-27 16:42:09 +0000

    Added some tests to the main method in the Student class.

--- Student.py
+++ Student.py
@@ -49,9 +49,19 @@ def main():
     student.setStudentID("1123465")
     print(f"Student ID: {student.getStudentID()}")
 
-    student.addCourse("MATH223", "DCIT201", "DCIT205", "CBAS210", "DCIT203", "DCIT207")
-    #student.displayCourse()
+    print("---------------- ADDS ONE COURSE -------------------")
+    student.addCourse("MATH223")
+    student.displayCourse()
 
+    print("----------------- ADDS MANY COURSES -------------------")
+    student.addCourse("DCIT201", "DCIT205", "CBAS210", "DCIT203", "DCIT207")
+    student.displayCourse()
+
+    print("------------------ REMOVES ONE COURSE -------------------")
+    student.removeCourse("CBAS210")
+    student.displayCourse()
+
+    print("------------------ REMOVES MANY COURSES -------------------")
     student.removeCourse("CBAS210", "DCIT203", "DCIT207")
     student.displayCourse()
 
